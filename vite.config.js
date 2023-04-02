9c25a73b99dc7700e5604cc5abc23387d8ada947
Himanshu Kumar <hklohani@outlook.com>
2023-04-02 17:52:12 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: '/src',
+export default ({ mode }) => {
+  const isProduction = mode === 'production';
+  const publicPath = isProduction ? '/chatgpt-frontend/dist/' : '/';
+
+  return defineConfig({
+    base: publicPath,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        src: '/src',
+      },
     },
-  },
-  base: getBase(),
-});
-function getBase() {
-  if (process.env.NODE_ENV === 'production') {
-    return 'chatgpt-frontend/dist/';
-  } else {
-    return '/';
-  }
-}
+  });
+};
