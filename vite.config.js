5580cf4606e39b252c6cc163341750a230238998
Himanshu Kumar <hklohani@outlook.com>
2023-04-05 10:45:33 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default ({ mode }) => {
   const isProduction = mode === 'production';
   const publicPath = isProduction ? '/chatgpt-frontend/dist/' : '/';
 
   return defineConfig({
-    base: publicPath,
-    plugins: [
-      react(),
-      createHtmlPlugin({
-        minify: true, // minify the output HTML
-        inject: {
-          // inject a custom function to modify the generated HTML
-          injectData: {
-            // define the path replacements here
-            replacements: [{ search: '/chatgpt-frontend/dist/', replace: './' }],
-          },
-          // define a custom function to modify the generated HTML
-          tags: () => {
-            return [
-              {
-                tag: 'link',
-                attrs: {
-                  rel: 'icon',
-                  type: 'image/svg+xml',
-                  href: '/chatgpt-frontend/dist/vite.svg',
-                },
-              },
-            ];
-          },
-        },
-      }),
-    ],
+    base: './',
+    plugins: [react()],
     resolve: {
       alias: {
         src: '/src',
